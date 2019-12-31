eae3b332ca645d1bf4b049cc1c0649d540012dcf
DBarinovv <9653314755@bk.ru>
2019-12-31 07:57:09 +0300

    With lines with 3 symbols

--- It_works.cpp
+++ It_works.cpp
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <math.h>
+
 //=============================================================================
 
 const int C_max_len_of_str = 20;
@@ -14,33 +16,60 @@ const int C_max_len_of_arr = 50;
 
 //=============================================================================
 
-char *Make_Right_Str  (const char *string);
+char *Make_Right_Str  (const char *string, int *if_short_string);
 
 int Case_Short_String (const char *string);
 
-char *Transform_Int_To_Short_String (int val);
+//=============================================================================
 
-//-----------------------------------------------------------------------------
+int main ()
+{
+    char** arr = (char **) calloc (C_max_len_of_arr, sizeof (char *));
+    for (int i = 0; i < C_max_len_of_arr; i++)
+    {
+        arr[i] = (char *) calloc (C_max_len_of_str, sizeof (char));
+    }
+
+    char *str = (char *) calloc (10, sizeof (char));
+
+    for (int i = 0; i < 5; i++)
+    {
+        gets (str);
+
+        int if_short_string = 0;
+        if (Make_Right_Str (str, &if_short_string) == nullptr)
+        {
+            arr[0] = (char *) if_short_string;
+            long long int helper = (long long int) if_short_string;
+//            printf ("HELPER = [%llu]\n", helper);
+//            printf ("MOd = [%d]\n", (int) floor (pow (256, 4)));
+//            helper %= (int) floor (pow (256, 4));
+//            printf ("HELPER = [%llu]\n", helper);
+//            helper += pow (256, 4);
+//            printf ("HELPER = [%llu]\n", helper);
+
+            printf ("String = (%s)\n", (char *) &helper);
+        }
+        else
+        {
+            arr[0] = str;
+        }
+    }
+
+    return 0;
+}
+
+//=============================================================================
 
 char *Make_Right_Str (const char *string, int *if_short_string)
 {
-//    printf ("!!!!!!!!!!!!!!!!\nSTRING = [%d]\n!!!!!!!!!!!!!!!!\n\n", strlen (string));
-//    printf ("SIZEOF INT = [%d]\n", sizeof (int));
-
-    if (strlen (string) + 1 >= sizeof (int)) // +1 because we need special elem
+    if (strlen (string) >= sizeof (int)) // +1 because we need special elem
     {
         return (char *) string;
     }
     else
     {
-        int val = Case_Short_String (string);
-
-        *if_short_string = val;
-
-//        char *helper = (char *) calloc (C_max_len_of_str, sizeof (char));
-//        helper = Transform_Int_To_Short_String (val);
-//
-//        printf ("\n!POG!\nSTR = [%s]\n", helper);
+        *if_short_string = Case_Short_String (string);
 
         return nullptr;
     }
@@ -59,58 +88,5 @@ int Case_Short_String (const char *string)
         pow *= 256;
     }
 
-    printf ("RES = [%d]\n", ptr);
-
     return ptr;
-}
-
-char *Transform_Int_To_Short_String (int val)
-{
-    char *res = (char *) calloc (C_max_len_of_str, sizeof (char));
-
-    val /= 256;   // skip special symbol
-
-//    for (int i = sizeof (int) - 2; i >= 0; i--)  // -2 because we skip one symbol
-//    {
-//        res[sizeof (int) - i - 2] = val % 256;
-//        val /= 256;
-//    }
-
-    res = (char *) &val;
-
-    for (int i = 0; i < strlen (res); i++)
-    {
-        printf ("Elem = (%c)\n", res[i]);
-    }
-
-    return res;
-}
-
-int main ()
-{
-    char** arr = (char **) calloc (C_max_len_of_arr, sizeof (char *));
-    for (int i = 0; i < C_max_len_of_arr; i++)
-    {
-        arr[i] = (char *) calloc (C_max_len_of_str, sizeof (char));
-    }
-
-    char *str = (char *) calloc (10, sizeof (char));
-
-    gets (str);
-
-    int if_short_string = 0;
-    if (Make_Right_Str (str, &if_short_string) == nullptr)
-    {
-        arr[0] = (char *) if_short_string;
-        Transform_Int_To_Short_String (if_short_string);
-    }
-    else
-    {
-        arr[0] = str;
-    }
-
-//    printf ("STR = [%s]\nSIZE = [%d]\n", str, strlen (str));
-
-
-    return 0;
 }
